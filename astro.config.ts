094de0dd9825ad7e72e629f78e7f849620066e0b
Golota Danil <7info7web@gmail.com>
2023-10-20 16:55:34 +0600

    fix: text + adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,37 +1,35 @@
 import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro'
-import Inspect from 'vite-plugin-inspect'
+import UnoCSS from 'unocss/astro';
 import sitemap from '@astrojs/sitemap';
 import vue from "@astrojs/vue";
-import nodejs from '@astrojs/node';
+// import nodejs from '@astrojs/node';
+// import Inspect from 'vite-plugin-inspect';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-    server: { host: '0.0.0.0', port: 3000 },
-    adapter: nodejs({
-        mode: 'standalone'
-    }),
-    output: 'hybrid',
-    compressHTML: true,
-    integrations: [
-        UnoCSS({
-            injectReset: true
-        }),
-        sitemap(),
-        vue({
-            appEntrypoint: '/src/_app'
-        }),
-    ],
-    vite: {
-        css: {
-            devSourcemap: true
-        },
-        plugins: [
-            vue(),
-            // Inspect({
-            //     build: true,
-            //     outputDir: '.vite-inspect'
-            // })
-        ],
-    }
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  adapter: netlify(),
+  output: 'server',
+  compressHTML: true,
+  integrations: [UnoCSS({
+    injectReset: true
+  }), sitemap(), vue({
+    appEntrypoint: '/src/_app'
+  })],
+  vite: {
+    css: {
+      devSourcemap: true
+    },
+    plugins: [vue()
+    // Inspect({
+    //     build: true,
+    //     outputDir: '.vite-inspect'
+    // })
+    ]
+  }
 });
